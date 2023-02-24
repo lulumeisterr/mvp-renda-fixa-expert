007c9186970d0f1f45f4a4043076ae5a3a28679d
Lucas <lucasrodriguesdonascimento@outlook.com>
2023-02-23 23:20:30 -0300

    Ajustes de regras

--- RendaFixaExpert/Program.cs
+++ RendaFixaExpert/Program.cs
@@ -11,15 +11,15 @@ decimal valorDecimal1, valorDecimal2 = 0;
 string valorFormatadoValorAnterior = "";
 string valorNovoInvestimento = "";
 string valorFormatadoTaxaRendimento = "";
+Dictionary<int, decimal> rentabilidadePorPeriodo = new Dictionary<int, decimal>();
 do
 {
     ZerarVariaveis();
     Console.WriteLine("");
     Console.WriteLine("################################");
     Console.WriteLine("Digite 1 - Quanto meus investimentos rendeu ?");
-    Console.WriteLine("Digite 2 - Qual foi minha rentabilidade ?");
-    Console.WriteLine("Digite 3 - Rentabilidade Acumulada entre periodos?");
-    Console.WriteLine("Digite 4 - Sair ?");
+    Console.WriteLine("Digite 2 - Calcular minhas rentabilidades ?");
+    Console.WriteLine("Digite 3 - Sair ?");
     Console.WriteLine("################################");
     Console.WriteLine("");
 
@@ -39,21 +39,53 @@ do
             break;
 
         case "2":
-            Console.WriteLine("Qual foi o seu ultimo valor investido ?");
-            valorFormatadoValorAnterior = FormatadorDecimal(Console.ReadLine(), "N2");
 
-            Console.WriteLine("Qual é o novo para ser investido valor ?");
-            valorNovoInvestimento = FormatadorDecimal(Console.ReadLine(), "N2");
+            decimal rentabilidadeAcumulada = 1;
+            decimal valorAntigo, valorNovo, resultado;
+            List<string> periodos  = new List<string>();
 
-            if (decimal.TryParse(valorFormatadoValorAnterior, NumberStyles.Currency, culture, out valorDecimal1) && decimal.TryParse(valorNovoInvestimento, NumberStyles.Currency, culture, out valorDecimal2))
-                Console.WriteLine($"Rentabilidade de : {CalcularTaxaRentabilidadeEntreDoisValores(valorDecimal1, valorDecimal2).ToString("0.#")}%");
+            Console.WriteLine("Você quer ver/comparar sua rentabilidade de quantos dias ?");
+            var dias = int.Parse(Console.ReadLine());
+
+            AdicionandoPeriodos(dias,ref periodos);
+
+            for (int g = 1; g < periodos.Count; g++)
+            {
+                if (g == 1)
+                {
+                    valorAntigo = decimal.Parse(periodos[g - 1]);
+                    valorNovo = decimal.Parse(periodos[g]);
+                    resultado = CalcularTaxaRentabilidadeEntreDoisValores(valorAntigo, valorNovo);
+                    rentabilidadePorPeriodo.Add(g, resultado);
+                    continue;
+                }
+                valorAntigo = decimal.Parse(periodos[g - 1]);
+                valorNovo = decimal.Parse(periodos[g]);
+
+                resultado = CalcularTaxaRentabilidadeEntreDoisValores(valorAntigo, valorNovo);
+                rentabilidadePorPeriodo.Add(g, resultado);
+            }
+
+            for (int j = 1; j < rentabilidadePorPeriodo.ToList().Count; j++)
+            {
+                rentabilidadeAcumulada = CalcularRentabilidadeAcumuladaTodosPeriodos(rentabilidadePorPeriodo, j);
+            }
+
+            // Cabecalho
+            Console.WriteLine($"{PadRight("Dias", 15)}{PadRight("Valor", 15)}{PadRight("Rentabilidade", 15)}");
+            var dictWithIndex = rentabilidadePorPeriodo.Select((item, index) => new { Index = index, Key = item.Key, Value = item.Value });
+
+            // Exibir os valores em loop
+            foreach (var rentabilidade in dictWithIndex)
+            {
+                Console.WriteLine($"{PadRight($"{rentabilidade.Key}", 15)}{PadRight($"{periodos[rentabilidade.Index]}", 15)}{PadRight($"{rentabilidade.Value.ToString("0.#")}%", 15)}");
+            }
+
+            Console.WriteLine($"Rentabilidade acumulada de todos os periodos adicionados: {rentabilidadeAcumulada.ToString("0.#")}%");
+
             break;
 
-
         case "3":
-
-
-        case "4":
             stopLoop = true;
             break;
 
@@ -65,7 +97,31 @@ do
 
 decimal CalcularTaxaRentabilidadeEntreDoisValores(decimal valorAnterior, decimal valorNovo) => ((valorNovo / valorAnterior) - 1) * 100;
 decimal CalcularAcumuloTaxa(decimal valorInvestido, decimal taxaRendimento) => valorInvestido * (1 + (taxaRendimento / 100));
+decimal CalcularRentabilidadeAcumuladaTodosPeriodos(Dictionary<int, decimal> rentabilidadePorPeriodo, int j)
+{
+    decimal rentabilidadeAcumulada;
+    var x = (rentabilidadePorPeriodo.Values.ToList()[j - 1] * rentabilidadePorPeriodo.Values.ToList()[j] + 1) - 1;
+    rentabilidadeAcumulada = Math.Abs(x);
+    return rentabilidadeAcumulada;
+}
+
 string FormatadorDecimal(string valor, string formato) => decimal.Parse(valor, culture).ToString(formato, culture);
+
+void AdicionandoPeriodos(int dias,ref List<string> periodos)
+{
+    var aux = 1;
+    for (int i = 0; i < dias; i++)
+    {
+        if (i >= aux)
+            aux = i + 1;
+        Console.WriteLine($"Adicione o {aux}º valor ?");
+        var valor = FormatadorDecimal(Console.ReadLine(), "N2");
+        periodos.Add(valor);
+    }
+}
+// Função para preencher uma string com espaços à direita para atingir a largura especificada
+static string PadRight(string input, int width) => input.PadRight(width, ' ');
+
 void ZerarVariaveis()
 {
     valorDecimal1 = 0;
@@ -73,4 +129,5 @@ void ZerarVariaveis()
     valorFormatadoTaxaRendimento = "";
     valorFormatadoValorAnterior = "";
     result = 0;
+    rentabilidadePorPeriodo = new Dictionary<int, decimal>();
 }
